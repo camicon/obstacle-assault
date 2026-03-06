69e2c4b7703dce54417d092fa5a8179ae7a7ef98
Cameron Conrad <camicon89@gmail.com>
2026-03-06 14:59:59 -0500

    Removed unwanted files marked in .gitignore

--- ObstacleAssault/Intermediate/Build/Win64/x64/ObstacleAssaultEditor/Development/Engine/SharedPCH.Engine.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "D:/Unreal/Udemy/obstacle-assault/ObstacleAssault/Intermediate/Build/Win64/x64/ObstacleAssaultEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.h"
-#include "Runtime/Engine/Public/EngineSharedPCH.h"
--- ObstacleAssault/Intermediate/Build/Win64/x64/ObstacleAssaultEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "D:/Unreal/Udemy/obstacle-assault/ObstacleAssault/Intermediate/Build/Win64/x64/ObstacleAssaultEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
