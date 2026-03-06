a97b231d3f3eca5d00b990e5efba171de837ed72
Cameron Conrad <camicon89@gmail.com>
2026-03-06 14:59:59 -0500

    Removed unwanted files marked in .gitignore

--- ObstacleAssault/Intermediate/Build/Win64/x64/ObstacleAssaultEditor/Development/CoreUObject/SharedPCH.CoreUObject.RTTI.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h
-#include "D:/Unreal/Udemy/obstacle-assault/ObstacleAssault/Intermediate/Build/Win64/x64/ObstacleAssaultEditor/Development/CoreUObject/SharedDefinitions.CoreUObject.RTTI.Cpp20.h"
-#include "Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h"
