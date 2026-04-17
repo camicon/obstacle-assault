c891e69dc48b641830d456f890de596a8f486787
Cameron Conrad <camicon89@gmail.com>
2026-04-17 14:09:58 -0400

    Corrected overshoot

--- ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
+++ ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
@@ -42,8 +42,12 @@ void AMovingPlatform::MovePlatform(float DeltaTime)
 
 	if (DistanceMoved >= MoveDistance)
 	{
+		FVector MoveDirection = PlatformVelocity.GetSafeNormal();
+		FVector NewStartLocation = StartLocation + MoveDirection * MoveDistance;
+		SetActorLocation(NewStartLocation);
+		StartLocation = NewStartLocation;
+
 		PlatformVelocity *= -1;
-		StartLocation = CurrentLocation;
 	}
 }
 
